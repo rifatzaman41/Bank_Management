644d4ab2a0a8067fdd8f4f46e5c5736156933ea1
rifatzaman41 <127728672+rifatzaman41@users.noreply.github.com>
2025-08-09 21:52:52 +0600

    up

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import environ
 env = environ.Env()
-environ.Env.read_env()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+#environ.Env.read_env()
+env.read_env(BASE_DIR / ".env")
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
 # Your secret key
